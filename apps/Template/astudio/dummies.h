7f0e5f768e5a893eb2f01a8333ecd9db3a289b96
AlexSC <asylvaincameron@gmail.com>
2018-07-31 19:55:56 -0400

    Terminer construire paquet

--- apps/Template/astudio/dummies.h
+++ apps/Template/astudio/dummies.h
@@ -9,7 +9,24 @@
 #ifndef DUMMIES_H_
 #define DUMMIES_H_
 
+char Error[] = "Vous n'avez pas les droits pour écrire dans le bracelet";
+char End[] = "End";
+
 //TODO: Create a CIVILIAN DUMMY
+char Nom[] = "Sylvain-Cameron";
+char Prenom[] = "Alexandre";
+char Date_Naissance[] = "1994-04-06";
+char Adresse[] = "2145 Rue Galt Ouest";
+char Appartement[] = "211";
+char Ville[] = "Sherbrooke";
+char Pays[] = "Canada";
+char Province[] = "Québec";
+char Code_Postale[] = "J1K1K3";
+char Soin_Particulier[] = "";
+char Medicament[] = "";
+char Maladie[] = "";
+char Commentaire[] = "";
+char Groupe_Sanguin[] = "";
 
 //TODO: Create a PROFESSIONAL DUMMY
 
