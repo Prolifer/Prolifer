38fb1f09ab0f846c2a07cea835a7a8d6894362bb
AlexSC <asylvaincameron@gmail.com>
2018-08-01 13:48:40 -0400

    small change

--- apps/Template/astudio/dummies.h
+++ apps/Template/astudio/dummies.h
@@ -12,20 +12,20 @@
 char Error[] = "Vous n'avez pas les droits pour écrire dans le bracelet";
 
 //TODO: Create a CIVILIAN DUMMY
-char Nom[20] = "Sylvain-Cameron";
-char Prenom[20] = "Alexandre";
-char Date_Naissance[15] = "1994-04-06";
-char Adresse[40] = "2145 Rue Galt Ouest";
-char Appartement[6] = "211";
-char Ville[20] = "Sherbrooke";
-char Pays[15] = "Canada";
-char Province[20] = "Québec";
-char Code_Postale[9] = "J1K1K3";
-char Soin_Particulier[40] = "";
-char Medicament[40] = "";
-char Maladie[40] = "";
-char Commentaire[40] = "";
-char Groupe_Sanguin[5] = "";
+char Nom[] = "Sylvain-Cameron";
+char Prenom[] = "Alexandre";
+char Date_Naissance[] = "1994-04-06";
+char Adresse[] = "2145 Rue Galt Ouest";
+char Appartement[] = "211";
+char Ville[] = "Sherbrooke";
+char Pays[] = "Canada";
+char Province[] = "Québec";
+char Code_Postale[] = "J1K1K3";
+char Soin_Particulier[] = "";
+char Medicament[] = "";
+char Maladie[] = "";
+char Commentaire[] = "";
+char Groupe_Sanguin[] = "";
 
 //TODO: Create a PROFESSIONAL DUMMY
 
